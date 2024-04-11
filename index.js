b9ba19976aec7e11ed5ca91b04b1bee39104866e
PohShiSien <pohshisien@gmail.com>
2024-04-11 10:34:16 +0800

    done

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function toggleNavbar() {
     body.classList.toggle("hidden");
     navBar.classList.toggle("hidden");
     let img = button.querySelector("img");
-    if (img.getAttribute("src") === "assets/images/icon-menu.svg") {
-        img.src = "assets/images/icon-menu-close.svg"
+    if (img.getAttribute("src") === "news-homepage-main/assets/images/icon-menu.svg") {
+        img.src = "news-homepage-main/assets/images/icon-menu-close.svg"
     } else {
-        img.src = "assets/images/icon-menu.svg"
+        img.src = "news-homepage-main/assets/images/icon-menu.svg"
     };
 }
 
